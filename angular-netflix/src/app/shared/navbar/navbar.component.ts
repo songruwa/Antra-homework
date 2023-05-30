a92a446743edf10c894e11ac748dbb76b6597ee6
Songru Wang <michaelsong6918@gmail.com>
2023-05-29 22:14:11 -0700

    small edit; replace 'movie list' with username in the navbar

--- angular-netflix/src/app/shared/navbar/navbar.component.ts
+++ angular-netflix/src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(public authService: AuthService) { }
+export class NavbarComponent implements OnInit {
+
+  public accessTToken: string | null | undefined;
+  public decodedTToken: any;
+  public userrrName: any;
+
+  constructor(public authService: AuthService, public jwthelper: JwtHelperService) { }
+
+  ngOnInit(): void {
+    this.accessTToken = localStorage.getItem('access_token');
+    if (this.accessTToken) {
+      this.decodedTToken = this.jwthelper.decodeToken(this.accessTToken);
+      this.userrrName = this.decodedTToken.username;
+      console.log(this.userrrName);
+
+    }
+  }
+
 
   logout() {
     this.authService.logout();
--- angular-netflix/src/app/shared/shared.module.ts
+++ angular-netflix/src/app/shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavbarComponent} from './navbar/navbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { CoreModule } from '../core/core.module';
-
+import { JwtModule, JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt'
 
 
 @NgModule({
@@ -13,11 +13,15 @@ import { CoreModule } from '../core/core.module';
   imports: [
     CommonModule,
     RouterModule,
-    CoreModule.forRoot(),
+    CoreModule.forRoot()
   ],
+  // https://stackoverflow.com/questions/49739277/nullinjectorerror-no-provider-for-jwthelperservice
+  providers: [
+    { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
+    JwtHelperService],
   exports: [
     NavbarComponent
   ],
-  
+
 })
 export class SharedModule { }
